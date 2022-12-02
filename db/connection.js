894cc042b0496059f82f24465a1c3f040a1a29a0
KIVA3 <kevinthinhtran1@gmail.com>
2022-12-02 16:10:35 -0700

    Created New Files
    Databases root files and basic framework of the homework

---
+++ db/connection.js
@@ -0,0 +1,13 @@
+const mysql = require("mysql2");
+const connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "Kevin20044!",
+  database: "employee_db",
+});
+
+connection.connect(function (err) {
+  if (err) throw err;
+});
+
+module.exports = connection;
